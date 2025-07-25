2218747122a78c1f42bb2fa5a69fc1020d4eb99a
MatheusBlanco <msallesblanco@gmail.com>
2025-07-25 11:47:01 -0300

    feat: :sparkles: Creates register and login

--- Sonora_REST_API/settings.py
+++ Sonora_REST_API/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
@@ -51,11 +52,23 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'dj_rest_auth.registration',
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.github',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.twitter',
 ]
+
+SITE_ID = 1
+
+# Authentication backends for django-allauth email authentication
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 
 MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
@@ -163,7 +176,13 @@ SIMPLE_JWT = {
 # dj-rest-auth
 REST_AUTH = {
     "USE_JWT": True,
-    "JWT_AUTH_COOKIE": "_auth",  # Name of access token cookie
-    "JWT_AUTH_REFRESH_COOKIE": "_refresh",  # Name of refresh token cookie
+    # "JWT_AUTH_COOKIE": "_auth",  # Name of access token cookie
+    # "JWT_AUTH_REFRESH_COOKIE": "_refresh",  # Name of refresh token cookie
     "JWT_AUTH_HTTPONLY": False,  # Makes sure refresh token is sent
 }
+
+
+ACCOUNT_AUTHENTICATION_METHOD = "email"  # Use Email / Password authentication
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = "none"  # Do not require email confirmation
--- Sonora_REST_API/urls.py
+++ Sonora_REST_API/urls.py
@@ -20,4 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/auth/", include("dj_rest_auth.urls")),
+    path('api/v1/auth/registration/', include('dj_rest_auth.registration.urls'))
+
 ]
