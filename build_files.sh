4e008d1549276496c88d93c06f998f60f55cf18f
MatheusBlanco <msallesblanco@gmail.com>
2025-08-14 14:32:16 -0300

    a

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
  python3 -m venv venv
- source /venv/bin/activate
+ source venv/bin/activate
  pip3 install -r requirements.txt
  python3 manage.py collectstatic --noinput --clear
  echo "BUILD END"
