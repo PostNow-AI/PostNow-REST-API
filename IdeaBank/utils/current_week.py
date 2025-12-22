5db0cfe2574b2a9225d093ffb8444d9d2d7c006f
MatheusBlanco <msallesblanco@gmail.com>
2025-12-22 18:04:49 -0300

    Fixes week ID calculation by adjusting weekday index to start from 1

--- IdeaBank/utils/current_week.py
+++ IdeaBank/utils/current_week.py
@@ -4,6 +4,6 @@ from datetime import date
 
 def get_current_week() -> str:
     current_week = datetime.datetime.now().isocalendar()[1]
-    today_number = date.today().weekday()
+    today_number = date.today().weekday() + 1
     week_id = str(today_number) + '_week_' + str(current_week)
     return week_id
